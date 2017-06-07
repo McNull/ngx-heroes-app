b53ca01e6aae96e88ad9fcf2f1654debd1d2b0f2
Bryan Slop <bd.slop@mindef.nl>
2017-06-07 12:35:53 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HeroService } from './services/hero/hero.service';
+import { Hero } from './models/hero';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  providers: [HeroService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  constructor(private heroService: HeroService) {}
+
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,11 @@
+import { Hero } from './../models/hero';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  @Input() hero: Hero;
+}
